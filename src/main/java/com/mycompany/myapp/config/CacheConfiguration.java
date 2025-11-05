8c811fc2697e21166408750ff469cbc67f9bd368
entsimsan <sm.sanna@entando.com>
2025-11-05 16:03:01 +0100

    Initial commit for Entando bundle v1 jhipster based

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -43,6 +43,7 @@ public class CacheConfiguration {
     public JCacheManagerCustomizer cacheManagerCustomizer() {
         return cm -> {
             createCache(cm, "oAuth2Authentication");
+            createCache(cm, com.mycompany.myapp.domain.Conference.class.getName());
             // jhipster-needle-caffeine-add-entry
         };
     }
