bd960516902039e3604100910a903604a0e39838
entsimsan <sm.sanna@entando.com>
2025-11-05 17:50:52 +0100

    version 0.0.8

---
+++ ui/widgets/conference/formWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,6 @@
+const conferenceMock = {
+  name:
+    'Consequatur omnis sed est atque nihil sed. Debitis nihil est et nulla vitae. Laboriosam exercitationem ea provident dolores incidunt suscipit in et. Cumque impedit et deleniti maxime dolorem. Numquam quia iusto totam quam dolorem facere magnam ratione possimus.',
+};
+
+export default conferenceMock;
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,14 @@
+const conferenceMocks = [
+  {
+    id: 0,
+    name:
+      'Consequatur omnis sed est atque nihil sed. Debitis nihil est et nulla vitae. Laboriosam exercitationem ea provident dolores incidunt suscipit in et. Cumque impedit et deleniti maxime dolorem. Numquam quia iusto totam quam dolorem facere magnam ratione possimus.',
+  },
+  {
+    id: 1,
+    name:
+      'Nostrum nam autem et labore molestiae omnis deleniti. Dignissimos consequatur porro optio soluta accusantium ipsum maxime dicta. Et voluptatem pariatur voluptatum. Eum in officia et in molestias. Alias dolorem aperiam id quae neque dolorem doloremque enim.',
+  },
+];
+
+export default conferenceMocks;
